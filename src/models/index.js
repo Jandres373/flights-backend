03cce73518d39bcb2ef1ca6ce26bec9f366a8d76
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2023-11-13 21:24:21 -0500

    modification v2

--- src/models/index.js
+++
@@ -1,9 +0,0 @@
-import User from './user.model'
-import EmailCode from './emailCode.model'
-import PasswordCode from './passwordCode.model'
-
-User.hasMany(EmailCode)
-EmailCode.belongsTo(User)
-
-User.hasMany(PasswordCode)
-PasswordCode.belongsTo(User)
--- src/routes/login.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const login_controller_1 = require("../controllers/login.controller");
-const loginRouter = (0, express_1.Router)();
-loginRouter.route('/')
-    .post(login_controller_1.loginUser);
-exports.default = loginRouter;
