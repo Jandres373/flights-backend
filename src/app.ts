0f2ffb6650d6ee97645994fbd2121790f2c26d31
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2023-11-14 00:48:21 -0500

    fixed CORS in backend

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import router from './routes';
 const app = Express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Reemplaza '*' con tu dominio permitido en producción
+  res.header('Access-Control-Allow-Origin', 'https://flights-front-jandres373.vercel.app'); 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
