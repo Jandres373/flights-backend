41ad00f5181adf99cbfa8f4852eb908929ee4423
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2023-11-14 01:06:33 -0500

    installed and adapted cors

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
 import Express from 'express'
 import router from './routes';
+import cors from 'cors';
+
 
 const app = Express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://flights-front-jandres373.vercel.app'); 
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  if (req.method === 'OPTIONS') {
-    res.status(200).send();
-  } else {
-    next();
-  }
-});
+app.use(cors({
+  origin: 'https://flights-front-jandres373.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+  credentials: true,
+}));
+
 
 app.use(Express.json()); 
 
