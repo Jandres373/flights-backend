1c55885ebfb35610c76ce446a5c14df50a242a16
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2023-11-14 00:55:59 -0500

    added options to the middleware and response 200

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,13 @@ const app = Express();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://flights-front-jandres373.vercel.app'); 
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+  if (req.method === 'OPTIONS') {
+    res.status(200).send();
+  } else {
+    next();
+  }
 });
 
 app.use(Express.json()); 
