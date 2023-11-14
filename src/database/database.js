498bf8813b8fed928fdad874ddc97fcc38171bcb
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2023-11-13 19:07:03 -0500

    modified port

--- src/database/database.js
+++ src/database/database.js
@@ -6,16 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+if (!process.env.DATABASE || !process.env.USER || !process.env.PASSWORD || !process.env.HOST || !process.env.PORT) {
+    throw new Error('Faltan variables de entorno requeridas');
+}
 const DATABASE = process.env.DATABASE;
 const USERNAME = process.env.USER;
 const PASSWORD = process.env.PASSWORD;
 const HOST = process.env.HOST;
+const PORT = parseInt(process.env.PORT) || 5432;
 const sequelize = new sequelize_1.Sequelize({
     database: DATABASE,
     username: USERNAME,
     password: PASSWORD,
     host: HOST,
-    port: 5432,
+    port: PORT,
     dialect: "postgres",
     dialectOptions: {
         ssl: {
--- src/database/database.ts
+++ src/database/database.ts
@@ -3,17 +3,22 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+if (!process.env.DATABASE || !process.env.USER || !process.env.PASSWORD || !process.env.HOST || !process.env.PORT) {
+  throw new Error('Faltan variables de entorno requeridas');
+}
+
 const DATABASE = process.env.DATABASE 
 const USERNAME = process.env.USER
 const PASSWORD = process.env.PASSWORD
 const HOST = process.env.HOST
+const PORT = parseInt(process.env.PORT) || 5432
 
 const sequelize = new Sequelize({
   database: DATABASE,
   username: USERNAME,
   password: PASSWORD,
   host: HOST,
-  port: 5432,
+  port: PORT,
   dialect: "postgres",
   dialectOptions: {
     ssl: {
