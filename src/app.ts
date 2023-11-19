384bbda5c5ecb43ec862aaa7457422e68857a6f8
Jandres373 <82553481+Jandres373@users.noreply.github.com>
2023-11-19 11:38:47 -0500

    backend 2.0 fixed

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,19 @@ import cors from 'cors';
 
 const app = Express();
 
-/* app.use(cors({
+app.use(cors({
   origin: 'https://flights-front-jandres373.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
   credentials: true,
-})); */
+}));
 
-app.use(cors({
+/* app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
   credentials: true,
-}));
+})); */
 
 app.use(Express.json()); 
 
--- src/database/database.ts
+++ src/database/database.ts
@@ -3,23 +3,27 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-if (!process.env.DATABASE || !process.env.USER || !process.env.PASSWORD || !process.env.HOST || !process.env.PORT) {
+/* if (!process.env.DATABASE || !process.env.USER || !process.env.PASSWORD || !process.env.HOST || !process.env.PORT) {
   throw new Error('Faltan variables de entorno requeridas');
-}
+} */
 
-const DATABASE = process.env.DATABASE 
+/* const DATABASE = process.env.DATABASE 
 const USERNAME = process.env.USER
 const PASSWORD = process.env.PASSWORD
 const HOST = process.env.HOST
-const PORT = parseInt(process.env.PORT) || 5432
+const PORT = parseInt(process.env.PORT) || 5432 */
 
-const sequelize = new Sequelize({
-  dialect: 'postgres',
-  host: HOST,
-  port: PORT,
-  username: USERNAME,
-  password: PASSWORD,
-  database: DATABASE,
+if (!process.env.DB) {
+  throw new Error('Faltan variables de entorno requeridas');
+}
+
+const DB = process.env.DB
+
+const sequelize = new Sequelize(DB,{
+  dialectOptions: {
+    keepAlive: true, 
+    keepAliveInterval: 30000 
+  },
   logging: false
 })
 
